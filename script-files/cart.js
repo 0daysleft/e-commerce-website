dfd378a79630c1002a83137a80107a506ed0f9d1
Vincent Okemwa <vincentarasa254@gmail.com>
2025-04-04 22:01:28 +0300

    Initialize cart.js with structure for cart functionality and sections

---
+++ script-files/cart.js
@@ -0,0 +1,14 @@
+
+
+
+//The Cart Have 6 Colums for Delete Item, Product Image, Product Name, Price Of Product, Quantity Of Products, Total Price of The Products
+
+//There is also a cart total Section Three Rows: Cart Sub Totals, Shipping Cost, Total(subtotals + shipping cost)
+
+//Coupon Section Also
+
+
+let cart = {
+
+}
+
