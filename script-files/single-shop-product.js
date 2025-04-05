8ad584dfa0196b289b348a80bccae3166a99c675
Vincent Okemwa <vincentarasa254@gmail.com>
2025-04-05 10:25:21 +0300

    Add product details section with images and description to single product page

--- script-files/single-shop-product.js
+++ script-files/single-shop-product.js
@@ -1,2 +1,55 @@
 
 
+
+/*
+    <section id="product-details" class="section-p1">
+        <div class="single-product-image" >
+            <img src="../Images/products/f1.jpg" width="100%" id="MainImg">
+
+            <div class="small-img-group">
+                <div class="small-img-col">
+                    <img src="../Images/products/f1.jpg" width="100%" class="small-img" alt="">
+                </div>
+
+                <div class="small-img-col">
+                    <img src="../Images/products/f2.jpg" width="100%" class="small-img" alt="">
+                </div>
+
+                <div class="small-img-col">
+                    <img src="../Images/products/f3.jpg" width="100%" class="small-img" alt="">
+                </div>
+
+                <div class="small-img-col">
+                    <img src="../Images/products/f4.jpg" width="100%" class="small-img" alt="">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="single-product-description" >
+            <h6>Home / T-Shirt</h6>
+            <h4>Men's Fashion T Shirt</h4>
+            <h2>$139.00</h2>
+            <select>
+                <option>Select Size</option>
+                <option>S</option>
+                <option>M</option>
+                <option>X</option>
+                <option>XL</option>
+                <option>XXL</option>  
+            </select>
+            <input type="number" value="3">
+            <button class="normal" >Add To Cart</button>
+            <h4>Product Details</h4>
+            <span>
+                The Gildan Ultron Cotton T-Shirt is a premium, high-quality tee designed for comfort, durability, and versatility. Made from a blend of cotton and polyester, it features a soft, breathable fabric that ensures all-day comfort. The t-shirt is designed with a modern, athletic fit, providing a flattering look for various body types. It has a smooth, consistent texture and a slightly tapered fit, which offers a sleek and contemporary appearance.
+            </span>
+        </div>
+    </section>
+*/
+
+document.getElementById("product-details")
+document.querySelector(".single-product-image");
+
+
+
