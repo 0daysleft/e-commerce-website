ddfc303c3f21e0c01885d15d6c956b74f1d8a187
odaysleft <vincentarasa254@gmail.com>
2025-05-18 14:06:29 +0300

    adding a if statement to check if the user is on the cart page

--- script-files/cart.js
+++ script-files/cart.js
@@ -1,1 +1,8 @@
-
+document.addEventListener('change', () => {
+if(document.getElementById('cartDetails')){
+     console.log(document.getElementById('cartDetails'))
+}
+else{
+     console.log('Not cart Page')
+}
+})
