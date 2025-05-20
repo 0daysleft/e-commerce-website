6243640e452d097868ee8053dc19a56c1efc4255
odaysleft <vincentarasa254@gmail.com>
2025-05-20 12:20:02 +0300

    commented out all the code

--- script-files/cart.js
+++ script-files/cart.js
@@ -1,16 +1,16 @@
-document.addEventListener('change', () => {
-if(document.getElementById('cartDetails')){
-     let c = document.querySelectorAll('#cartItemsCount')
-     //let v = c.reduce((item, total) => item + total)
-     c.forEach(
-          (item) => {
-               console.log(item.textContent)
-          }
-     )
-     //console.log(n)
-     //console.log("Total:",v)
-}
-else{
-     console.log('Not cart Page')
-}
-})
+// document.addEventListener('change', () => {
+// if(document.getElementById('cartDetails')){
+//      let c = document.querySelectorAll('#cartItemsCount')
+//      //let v = c.reduce((item, total) => item + total)
+//      c.forEach(
+//           (item) => {
+//                console.log(item.textContent)
+//           }
+//      )
+//      //console.log(n)
+//      // console.log("Total:",v)
+// }
+// else{
+//      console.log('Not cart Page')
+// }
+// })
