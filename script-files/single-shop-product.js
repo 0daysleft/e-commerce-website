7033fe22459c800576655620c9fb27a4d03c25b8
odaysleft <vincentarasa254@gmail.com>
2025-05-21 15:01:30 +0300

    refactor: update product price display to use original value for currency formatting

--- script-files/single-shop-product.js
+++ script-files/single-shop-product.js
@@ -243,7 +243,7 @@ function cart() {
                 row.appendChild(productNameCell);
 
                 const productPriceCell = document.createElement("td");
-                const productPrice = document.createTextNode(convertToLocaleCurrencyString(Number(elem.productPrice)))
+                const productPrice = document.createTextNode(convertToLocaleCurrencyString(elem.productPrice))
                 productPriceCell.appendChild(productPrice);
                 row.appendChild(productPriceCell);
 
