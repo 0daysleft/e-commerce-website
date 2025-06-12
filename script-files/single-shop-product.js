524c51cf769cb26d2292e158481b78ccd3c3f892
odaysleft <vincentarasa254@gmail.com>
2025-06-12 08:07:49 +0300

    refactor: simplify deleteProduct function to directly remove item from cart

--- script-files/single-shop-product.js
+++ script-files/single-shop-product.js
@@ -165,18 +165,18 @@ function updatePricesInLocaleString() {
     });
 }
 
-function deleteProduct() {
+function deleteProduct(cartNo) {
 
-    cartArray.forEach((value, index) => {
-        // cartArray.splice(index, 1)
-        // console.log("cartArray.splice(index, 1)", cartArray.splice(index, 1))
-        //console.log("Index:", value)
-        // console.log("Value:", value)
-        // sessionStorage.setItem("cart", JSON.stringify(cartArray))
-        // displayCart()
-        // updateCartQuantity()
+    //cartArray.forEach((value, index) => {
+    cartArray.splice(cartNo, 1)
+    // console.log("cartArray.splice(index, 1)", cartArray.splice(index, 1))
+    //console.log("Index:", value)
+    // console.log("Value:", value)
+    sessionStorage.setItem("cart", JSON.stringify(cartArray))
+    displayCart()
+    updateCartQuantity()
 
-    })
+    //})
 }
 
 function mainCart() {
