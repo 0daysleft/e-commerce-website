dcfbe5096ef8f09dadbcf9194d97fc96c42e88bf
odaysleft <vincentarasa254@gmail.com>
2025-06-17 18:28:01 +0300

    refactor: streamline cart update logic and improve display handling for empty cart

--- script-files/cart.js
+++ script-files/cart.js
@@ -35,6 +35,7 @@ function updatePricesInLocaleString() {
 function deleteProduct(cartNo) {
      cartArray.splice(cartNo, 1)
      sessionStorage.setItem("cart", JSON.stringify(cartArray))
+     updateCartPage()
      updatePricesInLocaleString();
      displayCart()
      updateCartQuantity()
@@ -47,117 +48,119 @@ function updateCartQuantity() {
      cartIcon.setAttribute("data-count", cartQuantity);
 }
 
-if (cartArray.length > 0 || cartArray == []) {
+function updateCartPage() {
+     if (cartArray.length > 0 || cartArray == []) {
 
-     displayCart = () => {
-          cartTableBody.innerHTML = ""; // Reset the table body
-          const fragment = document.createDocumentFragment();
-          cartArray.forEach((elem, index) => {
-               const row = document.createElement("tr");
+          displayCart = () => {
+               cartTableBody.innerHTML = ""; // Reset the table body
+               const fragment = document.createDocumentFragment();
+               cartArray.forEach((elem, index) => {
+                    const row = document.createElement("tr");
 
-               updatePrices = (totalQuantity) => {
-                    totalSingleItemPrice = (elem.productPrice * totalQuantity)
-               }
+                    updatePrices = (totalQuantity) => {
+                         totalSingleItemPrice = (elem.productPrice * totalQuantity)
+                    }
 
-               updatePrices(elem.productQuantity);
-               const removeCell = document.createElement("td");
-               removeCell.innerHTML = `<a href="#"><i class="fa-solid fa-times-circle"></i></a>`;
-               row.appendChild(removeCell);
-               removeCell.setAttribute('data-delete', index)
-               removeCell.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    deleteProduct(index)
+                    updatePrices(elem.productQuantity);
+                    const removeCell = document.createElement("td");
+                    removeCell.innerHTML = `<a href="#"><i class="fa-solid fa-times-circle"></i></a>`;
+                    row.appendChild(removeCell);
+                    removeCell.setAttribute('data-delete', index)
+                    removeCell.addEventListener('click', (e) => {
+                         e.preventDefault();
+                         deleteProduct(index)
 
-               })
+                    })
 
-               const imgCell = document.createElement("td");
-               const img = document.createElement("img");
-               img.src = elem.productImage;
-               img.alt = elem.productName;
-               imgCell.appendChild(img);
-               row.appendChild(imgCell);
+                    const imgCell = document.createElement("td");
+                    const img = document.createElement("img");
+                    img.src = elem.productImage;
+                    img.alt = elem.productName;
+                    imgCell.appendChild(img);
+                    row.appendChild(imgCell);
 
-               const productNameCell = document.createElement("td");
-               const productName = document.createTextNode(elem.productName)
-               productNameCell.setAttribute("class", 'product-name-cell')
-               productNameCell.appendChild(productName);
-               row.appendChild(productNameCell);
+                    const productNameCell = document.createElement("td");
+                    const productName = document.createTextNode(elem.productName)
+                    productNameCell.setAttribute("class", 'product-name-cell')
+                    productNameCell.appendChild(productName);
+                    row.appendChild(productNameCell);
 
-               const productPriceCell = document.createElement("td");
-               const productPrice = document.createTextNode(convertToLocaleCurrencyString(Number(elem.productPrice)))
-               productPriceCell.appendChild(productPrice);
-               row.appendChild(productPriceCell);
+                    const productPriceCell = document.createElement("td");
+                    const productPrice = document.createTextNode(convertToLocaleCurrencyString(Number(elem.productPrice)))
+                    productPriceCell.appendChild(productPrice);
+                    row.appendChild(productPriceCell);
 
-               const productQuantityCell = document.createElement("td");
+                    const productQuantityCell = document.createElement("td");
 
-               const quantityInput = document.createElement("input");
-               quantityInput.setAttribute('type', 'number');
-               quantityInput.setAttribute('class', 'updated-item-quantity');
-               quantityInput.setAttribute('min', '1');
-               quantityInput.setAttribute('max', '99');
-               quantityInput.value = Number(elem.productQuantity);
+                    const quantityInput = document.createElement("input");
+                    quantityInput.setAttribute('type', 'number');
+                    quantityInput.setAttribute('class', 'updated-item-quantity');
+                    quantityInput.setAttribute('min', '1');
+                    quantityInput.setAttribute('max', '99');
+                    quantityInput.value = Number(elem.productQuantity);
 
-               quantityInput.addEventListener('input', (e) => {
-                    let newQuantity = Number(e.target.value);
-                    elem.productQuantity = newQuantity;
-                    if (newQuantity > 99) {
-                         e.target.value = 99;
-                         elem.productQuantity = 99;
+                    quantityInput.addEventListener('input', (e) => {
+                         let newQuantity = Number(e.target.value);
+                         elem.productQuantity = newQuantity;
+                         if (newQuantity > 99) {
+                              e.target.value = 99;
+                              elem.productQuantity = 99;
+                              sessionStorage.setItem("cart", JSON.stringify(cartArray))
+                              alert('max value is 99')
+                              return
+                         }
+
+                         function updateCartRowPrice(elem, row) {
+                              const priceCell = row.querySelector('#cartTotalProductPrice span');
+                              const newTotal = elem.productPrice * elem.productQuantity;
+                              priceCell.textContent = newTotal.toLocaleString('en-KE', {
+                                   style: 'currency',
+                                   currency: "KES"
+                              });
+                         }
+
+                         // Update the cart array
                          sessionStorage.setItem("cart", JSON.stringify(cartArray))
-                         alert('max value is 99')
-                         return
-                    }
+                         // Recalculate totals and update UI
+                         updateCartQuantity()
+                         updatePrices(quantity);
+                         //addProductToCart()
+                         mainCart()
+                         updatePricesInLocaleString(); // Your function to recalculate totals
+                         updateCartRowPrice(elem, row); // Optional: update just this row’s total
+                    });
 
-                    function updateCartRowPrice(elem, row) {
-                         const priceCell = row.querySelector('#cartTotalProductPrice span');
-                         const newTotal = elem.productPrice * elem.productQuantity;
-                         priceCell.textContent = newTotal.toLocaleString('en-KE', {
-                              style: 'currency',
-                              currency: "KES"
-                         });
-                    }
-
-                    // Update the cart array
-                    sessionStorage.setItem("cart", JSON.stringify(cartArray))
-                    // Recalculate totals and update UI
-                    updateCartQuantity()
-                    updatePrices(quantity);
-                    //addProductToCart()
-                    mainCart()
-                    updatePricesInLocaleString(); // Your function to recalculate totals
-                    updateCartRowPrice(elem, row); // Optional: update just this row’s total
+                    productQuantityCell.appendChild(quantityInput);
+                    row.appendChild(productQuantityCell);
+                    const productSubtotalCell = document.createElement("td");
+                    const productSubtotal = document.createTextNode(convertToLocaleCurrencyString(totalSingleItemPrice))
+                    productSubtotalCell.appendChild(productSubtotal);
+                    row.appendChild(productSubtotalCell);
+                    fragment.appendChild(row);
                });
+               cartTableBody.appendChild(fragment);
+               updatePricesInLocaleString();
+          }
 
-               productQuantityCell.appendChild(quantityInput);
-               row.appendChild(productQuantityCell);
+          displayCart()
 
-
-               const productSubtotalCell = document.createElement("td");
-               const productSubtotal = document.createTextNode(convertToLocaleCurrencyString(totalSingleItemPrice))
-               productSubtotalCell.appendChild(productSubtotal);
-               row.appendChild(productSubtotalCell);
-               fragment.appendChild(row);
-          });
-          cartTableBody.appendChild(fragment);
-          updatePricesInLocaleString();
-     }
-     displayCart()
-
-} else {
-     cartVoucherAndSubtotal.style.display = 'none'
-     cartTableBody.innerHTML = ""; // Clear old rows
-     const emptyRow = document.createElement("tr");
-     const emptyCell = document.createElement("td");
-     emptyCell.colSpan = 6;
-     emptyCell.innerHTML = `
+     } else {
+          cartVoucherAndSubtotal.style.display = 'none'
+          cartTableBody.innerHTML = ""; // Clear old rows
+          const emptyRow = document.createElement("tr");
+          const emptyCell = document.createElement("td");
+          emptyCell.colSpan = 6;
+          emptyCell.innerHTML = `
                 <div style="padding: 20px; text-align: center; color: red; height: 40vh; display: flex; justify-content: center; align-items: center; flex-direction: column">
                      🥲 Your cart is empty! <br><br>
                     <a href="./shop.html" style="color: green;">Please Shop 🛍️</a>
                 </div>
             `;
-     emptyRow.appendChild(emptyCell);
-     cartTableBody.appendChild(emptyRow);
-     updatePricesInLocaleString();
+          emptyRow.appendChild(emptyCell);
+          cartTableBody.appendChild(emptyRow);
+          updatePricesInLocaleString();
+     }
 }
 
+updateCartPage()
 
