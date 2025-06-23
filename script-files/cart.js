f50d7ffb6c286d913bdf4fbb2cdc291014c9bda3
odaysleft <vincentarasa254@gmail.com>
2025-06-23 09:21:16 +0300

    Fix: remove unused opacity handling in empty cart alert function

--- script-files/cart.js
+++ script-files/cart.js
@@ -61,11 +61,6 @@ function updateCartQuantity() {
 }
 
 function displayEmptyCartAlert() {
-     const div = document.querySelector('.blog-header')
-     const style = window.getComputedStyle(div);
-     //style.style.opacity = '0.2'
-     console.log(style.opacity)
-
      document.querySelector('.blog-header h2').textContent = 'Ooops 🥲. Seems you landed on the wrong page!!'
      document.querySelector('.blog-header p').innerHTML = '<h3>Visit the <a href="../html-files/shop.html" style="text-decoration: none; color: yellow; background-color: green">shop page</a> to shop and then checkout here, see you here again!!</h3>'
      // Clear any previous rows in the cart body
